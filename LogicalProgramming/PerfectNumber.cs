a8529375b652bf425bfa6713d5f51e18859b04a3
unknown <mayurubale5335@gmail.com>
2023-04-21 20:32:41 +0530

    [mayur ubale] Modify Perfect Number

---
+++ LogicalProgramming/PerfectNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class PerfectNumber
+    {
+        public void CheckPerfect(int N)
+        {
+            int sum = 0;
+            for (int i = 1; i < N; i++)
+            {
+                if (N % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if (sum == N)
+            {
+                Console.WriteLine("{0} is Perfect Number", N);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Not Perfect Number", N);
+            }
+        }
+    }
+}
